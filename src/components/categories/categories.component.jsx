56d26e39d3a281da669f2e0d5b099c301e7974e3
sdendaa <sdendaa@gmail.com>
2022-05-13 20:06:51 -0400

    updating our categories and category-item component

---
+++ src/components/categories/categories.component.jsx
@@ -0,0 +1,15 @@
+import "./categories.style.scss";
+
+import CategoryItem from "../category-item/category-item.component";
+
+const Categories = ({ categories }) => {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default Categories;
